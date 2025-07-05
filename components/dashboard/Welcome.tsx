b53eba8372373e1e0d90ea920c97857e8016d572
Robert Cho <rob@Roberts-Mac-mini.local>
2025-07-05 11:50:21 -0400

    no commit message

--- components/dashboard/Welcome.tsx
+++ components/dashboard/Welcome.tsx
@@ -9,7 +9,7 @@ type WelcomeBoardProps = {
 
 export const WelcomeBoard: React.FC<WelcomeBoardProps> = ({ user, companyName }) => {
   return (
-    <div className='flex flex-col justify-between items-start bg-gradient-to-r from-[#0492cf] to-[#2cdbb2] border-2 border-white border-opacity-80 rounded-lg px-5 py-5 mr-4 mb-6 mt-10'>
+    <div className='flex flex-col justify-between items-start bg-gradient-to-r from-[#0492cf] to-[#2cdbb2] border-2 border-white border-opacity-80 rounded-lg px-5 py-5 mr-4 mb-6 mt-10 animate-fade-in'>
       <h2 className='text-2xl font-bold text-white'>
         Hello {user} ({companyName})
       </h2>
