4419bc30c7d0f3ef72ac446d29da52d6de169930
Robert Cho <rob@Roberts-Mac-mini.local>
2025-07-08 15:06:34 -0400

    no commit message

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true
+      }
+    ]
+  }
 };
 
 export default nextConfig;
